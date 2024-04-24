1ed0ca86633fdc524054128261436c3404eadcb8
RDixonCodes <rhdixon79@gmail.com>
2024-04-24 00:35:45 -0700

    rest client

---
+++ src/main/java/com/rickiedixon/runnerz/user/Address.java
@@ -0,0 +1,10 @@
+package com.rickiedixon.runnerz.user;
+
+public record Address(
+        String street,
+        String suite,
+        String city,
+        String zipcode,
+        Geo geo
+) {
+}
